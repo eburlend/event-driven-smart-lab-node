b5b1fc912754c2f57e515cabc6ed11f4b65d30b5
eburlend <eburlend@gmail.com>
2026-02-11 13:50:39 -0700

    dht11.c is complete

--- components/dht11/dht11.c
+++ components/dht11/dht11.c
@@ -26,6 +26,89 @@ static const char *TAG = "DHT11_WRAPPER"
 // Initialized to GPIO_NUM_NC (Not Connected) to indicate that
 // dht11_init() has not been called yet
 static gpio_num_t s_pin = GPIO_NUM_NC;
-//Elijah Burlend
 
 
+// Function
+// Convert a temperature value from Celsius to Fahrenheit
+// Declare static so it is only visible in this source file
+static float c_to_f(float tempc)
+{
+    return(tempc * 9.0f / 5.0f) + 32.0f;
+}
+
+// Initialize the DHT11 driver wrapper
+// This function records which GPIO pin the DHT11 line is 
+// connected to
+void dht11_init(gpio_num_t pin)
+{
+    // Store the provided GPIO pin in a static variable
+    s_pin = pin;
+    // %d: insert and integer here
+
+    ESP_LOGIC(TAG, "Initialized DHT11 on GPIO %d", (int)s_pin);
+}
+
+// Reads humidity and temperature
+// The results are written into the structure pointed by *out
+esp_err_t dht11_read(dht11_reading_t *out)
+{
+    // Check if dht11_int() was called
+    // If not, the GPIO is still marked as "not connected"
+    if(s_pin == GPIO_NUM_NC)
+    {
+        // Log the data and return fail
+        ESP_LOG(TAG, "dht11_inint() not called");
+        return ESP_FAIL;
+    }
+
+    // Validate the output pointer
+    // If it is NULL, we cannot safely write data to it
+    if(out == NULL)
+    {
+        return ESP_ERR_INVALID_ARG;
+
+    }
+
+    // Local vars to store the raw sensor readings
+    // f tells the compiler: float and not a double
+    float humidity =  0.0f;
+    float temperature_c = 0.0f;
+
+    // Call the DHT library to read humidity and temp
+    esp_err_t err = dht_read_float_data(
+        DHT_TYPE_DHT11,              // Specifies the sensor
+        s_pin,                       // GPIO pin
+        &humidity,                   // Address where humidity will be stored
+        &temperature_c               // Address where temp (degrees C)
+        
+    );
+
+    //check if the library call was successful
+    if(err != ESP_OK)
+    {
+        // Log an error
+        ESP_LOG(TAG, "dht_read_float_data failed");
+        return err;
+    }
+
+    // Store the humidity reading output structure
+    out->humidity = humidity;
+    // Store the temp in Celsius
+    out->temperature_c = temperature_c;
+    // Convert the temp to Fahrenheit
+    out->temperature_f = c_to_f(temperature_c);
+
+
+    // Indicate that the read was successful
+    return ESP_OK;
+
+    
+
+
+
+
+
+
+
+
+}
